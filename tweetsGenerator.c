9feb51bd26889c4e09c772dacfd7d24371dc1bd0
idopinto <ido.pinto@mail.huji.ac.il>
2021-04-26 16:24:43 +0300

    add tweetsGenerator.c and text

--- tweetsGenerator.c
+++ tweetsGenerator.c
@@ -1,6 +1,147 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define MAX_WORDS_IN_SENTENCE_GENERATION 20
+#define MAX_WORD_LENGTH 100
+#define MAX_SENTENCE_LENGTH 1000
+
+typedef struct WordStruct {
+  char *word;
+  struct WordProbability *prob_list;
+  //... Add your own fields here
+} WordStruct;
+
+typedef struct WordProbability {
+  struct WordStruct *word_struct_ptr;
+  //... Add your own fields here
+} WordProbability;
+
+/************ LINKED LIST ************/
+typedef struct Node {
+  WordStruct *data;
+  struct Node *next;
+} Node;
+
+typedef struct LinkList {
+  Node *first;
+  Node *last;
+  int size;
+} LinkList;
+
+/**
+ * Add data to new node at the end of the given link list.
+ * @param link_list Link list to add data to
+ * @param data pointer to dynamically allocated data
+ * @return 0 on success, 1 otherwise
+ */
+int add(LinkList *link_list, WordStruct *data)
+{
+  Node *new_node = malloc(sizeof(Node));
+  if (new_node == NULL)
+  {
+	return 1;
+  }
+  *new_node = (Node){data, NULL};
+
+  if (link_list->first == NULL)
+  {
+	link_list->first = new_node;
+	link_list->last = new_node;
+  }
+  else
+  {
+	link_list->last->next = new_node;
+	link_list->last = new_node;
+  }
+
+  link_list->size++;
+  return 0;
+}
+/*************************************/
+
+/**
+ * Get random number between 0 and max_number [0, max_number).
+ * @param max_number
+ * @return Random number
+ */
+int get_random_number(int max_number)
+{
+}
+
+/**
+ * Choose randomly the next word from the given dictionary, drawn uniformly.
+ * The function won't return a word that end's in full stop '.' (Nekuda).
+ * @param dictionary Dictionary to choose a word from
+ * @return WordStruct of the chosen word
+ */
+WordStruct *get_first_random_word(LinkList *dictionary)
+{
+}
+
+/**
+ * Choose randomly the next word. Depend on it's occurrence frequency
+ * in word_struct_ptr->WordProbability.
+ * @param word_struct_ptr WordStruct to choose from
+ * @return WordStruct of the chosen word
+ */
+WordStruct *get_next_random_word(WordStruct *word_struct_ptr)
+{
+}
+
+/**
+ * Receive dictionary, generate and print to stdout random sentence out of it.
+ * The sentence most have at least 2 words in it.
+ * @param dictionary Dictionary to use
+ * @return Amount of words in printed sentence
+ */
+int generate_sentence(LinkList *dictionary)
+{
+}
+
+/**
+ * Gets 2 WordStructs. If second_word in first_word's prob_list,
+ * update the existing probability value.
+ * Otherwise, add the second word to the prob_list of the first word.
+ * @param first_word
+ * @param second_word
+ * @return 0 if already in list, 1 otherwise.
+ */
+int add_word_to_probability_list(WordStruct *first_word,
+                                 WordStruct *second_word)
+{
+}
+
+/**
+ * Read word from the given file. Add every unique word to the dictionary.
+ * Also, at every iteration, update the prob_list of the previous word with
+ * the value of the current word.
+ * @param fp File pointer
+ * @param words_to_read Number of words to read from file.
+ *                      If value is bigger than the file's word count,
+ *                      or if words_to_read == -1 than read entire file.
+ * @param dictionary Empty dictionary to fill
+ */
+void fill_dictionary(FILE *fp, int words_to_read, LinkList *dictionary)
+{
+}
+
+/**
+ * Free the given dictionary and all of it's content from memory.
+ * @param dictionary Dictionary to free
+ */
+void free_dictionary(LinkList *dictionary)
+{
+}
+
+/**
+ * @param argc
+ * @param argv 1) Seed
+ *             2) Number of sentences to generate
+ *             3) Path to file
+ *             4) Optional - Number of words to read
+ */
+int main(int argc, char *argv[])
+{
+  return 0;
 }
